9e5b23e5a9bcb932697a81a49d96b6cda85d2960
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-17 16:14:14 +0100

    updated simulate.sh

--- simulate.sh
+++ simulate.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 # This script will be used to launch simulations 
+
+if [[ -e $1 ]] ; then
+	abs_path="$(cd "$(dirname $1)" && pwd)/$(basename "$1")"
+
+	cd cslp/bin
+	java cslp/Simulator $abs_path
+else
+	echo '"$1" does not exist; exiting.'
+	exit 1
+fi
