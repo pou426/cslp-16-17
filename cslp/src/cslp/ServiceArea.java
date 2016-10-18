41ac8039406ec1a09a3dedf293f0e833e456716c
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-18 12:09:59 +0100

    add print() method

--- cslp/src/cslp/ServiceArea.java
+++ cslp/src/cslp/ServiceArea.java
@@ -1,28 +1,29 @@
 package cslp;
 
+import java.util.Arrays;
+
 public class ServiceArea {
-	public int areaIdx; // unit8_t
-	// no. of trips per hour 
-	public double serviceFreq;
-	// a value between 0 and 1
-	// occupancy threshold triggers waste collection
-	public double thresholdVal;
-	// Number of bins in this area
-	public int noBins; // unit16_t
-	// matrix representation of the duration in mins between two locations in an area
-	// (0,0) corresponds to the depot
-	// while elements (i,j) are zero when i=j
-	// use -1 to indicate there is no direct link between two locations
-	// we use directed graphs i.e. there may only be a one way link between two vertices
-	public int[][] roadsLayout; // int8_t
+	public short areaIdx; // unit8_t
+	public float serviceFreq;
+	public float thresholdVal;
+	public short noBins; // unit16_t
+	public short[][] roadsLayout; // int8_t
 	
-	public ServiceArea(int areaIdx, double serviceFreq,
-			double thresholdVal, int noBins, int[][] roadsLayout) {
+	public ServiceArea(short areaIdx, float serviceFreq,
+			float thresholdVal, short noBins, short[][] roadsLayout) {
 		this.areaIdx = areaIdx;
 		this.serviceFreq = serviceFreq;
 		this.thresholdVal = thresholdVal;
 		this.noBins = noBins;
 		this.roadsLayout = roadsLayout;
+	}
+	
+	public void print() {
+		String s = "areaIdx= "+String.valueOf(areaIdx)+" serviceFreq= "+String.valueOf(serviceFreq)+
+				" thresholdVal= "+String.valueOf(thresholdVal)+" noBins= "+String.valueOf(noBins);
+		System.out.println(s);
+		System.out.println("roadsLayout");
+		System.out.println(Arrays.deepToString(roadsLayout));
 	}
 	
 }
