0c555bac5cd82d1a02853c6d4f611aa8c46b04c6
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-18 14:46:01 +0100

    parse all experiment keywords

--- cslp/src/cslp/Simulator.java
+++ cslp/src/cslp/Simulator.java
@@ -73,7 +73,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							noAreas = Short.parseShort(tokens[1]);
-							if (!noAreasFound)		noAreasFound = true;
+							noAreasFound = true;
 							
 							while ((!serviceAreasFound) && (line = br.readLine()) != null) {
 								if (!(line.startsWith("#") || line.isEmpty())) {
@@ -140,8 +140,95 @@ public class Simulator {
 						}
 						break;
 						
-					case "ServiceFreq":
+					case "serviceFreq":
+						if (noAreasFound) {
+							System.out.println("Warning: Multiple inputs for noAreas variable. Previously stored area information will be overwritten.");
+						} else if (!(tokens[1].equals("experiment"))) {
+							throw new InvalidInputFileException("Invalid format: missing keyword \"experiment\" in this line: " + line);
+						}
 						
+						int j = 2;
+						while (!(tokens[j].equals("noAreas"))) {
+							float serviceFreq = Float.parseFloat(tokens[j]);
+							serviceFreqExp.add(serviceFreq);
+							j++;
+						}
+						if (j <= 2) { 
+							throw new InvalidInputFileException("Insufficient inputs for the serviceFreq experiment parameters in this line: "+line);
+						} else {
+							isExperiment = true;
+							
+							noAreas = Short.parseShort(tokens[++j]);
+							noAreasFound = true;
+							
+							if (j != (tokensLen-1)) {
+								System.out.println("Warning: too many inputs for noAreas parameter");
+							}
+														
+							while ((!serviceAreasFound) && (line = br.readLine()) != null) {
+								if (!(line.startsWith("#") || line.isEmpty())) {
+									String[] areaTokens = line.split("\\s+");
+									if (!areaTokens[0].equals("areaIdx")) {
+										throw new InvalidInputFileException("Invalid format after noAreas line or insufficient service area specifications");
+									} else if (!((areaTokens.length == 8) && (areaTokens[2].equals("serviceFreq")) &&
+	                                        (areaTokens[4].equals("thresholdVal")) && (areaTokens[6].equals("noBins")))) {
+										throw new InvalidInputFileException("Invalid format for service area description line: " + line);
+									} else {
+										short areaIdx = Short.parseShort(areaTokens[1]);
+	                                    float serviceFreq = Float.parseFloat(areaTokens[3]);
+	                                    float thresholdVal = Float.parseFloat(areaTokens[5]);
+	                                    int noBins = Integer.parseInt(areaTokens[7]);
+	                                    if (noBins > 65535) {
+	                                    	throw new InvalidInputFileException("noBins exceeds maximum 65,535 in line: "+line);
+	                                    }
+	                                    int m = noBins + 1; 
+	                                    short[][] roadLayout = new short[m][m];
+	                                    boolean areaFound = false;
+	                                    
+	                                    while ((!areaFound) && (line = br.readLine()) != null) {
+	                                    	if (!(line.startsWith("#") || line.isEmpty())) {
+	                                    		String[] layoutTokens = line.split("\\s+");
+	                                    		if (!(layoutTokens[0].equals("roadsLayout"))) {
+	                                    			throw new InvalidInputFileException("Incorrect format: roadsLayout keyword not in the next line of the areaIdx line for areaIdx = " + areaIdx + ".");
+	                                    		} 
+	                                    		int count = 0;
+	                                    		while ((count < m) && ((line=br.readLine()) != null)) {
+	                                    			String[] matrixTokens = line.trim().split("\\s+");
+	                                    			if (!((matrixTokens[0].equals("#")) || line.isEmpty())) {
+	                                    				if (matrixTokens.length != m) {
+		                                                    throw new InvalidInputFileException("Incorrect number of row elements in roadlayout for areaIdx = " + areaIdx);
+	                                    				} else {
+	                                    					int index = 0;
+		                                                    for (int col = 0; col < m; col++) {
+		                                                        roadLayout[count][col] = Short.parseShort(matrixTokens[index]);                                                 
+		                                                        index++;
+		                                                    }
+		                                                    count++;
+		                                                }
+	                                    			}
+	                                    		}
+	                                            // check the matrix is diagonally zeros
+	                                            for (int d = 0; d < m; d++) {
+	                                                if (roadLayout[d][d] != 0) {
+	                                                    throw new InvalidInputFileException("Invalid format for the roadLayout matrix for areaIdx = " + areaIdx + ".");
+	                                                }
+	                                            }
+	                                            // check that sufficient area information has been obtained 
+	                                            if (count == m) {
+	                                                areaFound = true;
+	                                            } else {
+	                                                throw new InvalidInputFileException("Incorrect no. of columns for the roadLayout matrix for areaIdx = " + areaIdx + ".");
+	                                            }
+	                                    	}
+	                                    }
+	                                    ServiceArea curr_sa = new ServiceArea(areaIdx,serviceFreq,thresholdVal,noBins,roadLayout);
+	                                    serviceAreas.add(curr_sa);
+	                                    if (serviceAreas.size() == noAreas)		serviceAreasFound = true;
+									}
+								}
+							}
+						}
+						break;
 						
 					case "lorryVolume":
 						if (lorryVolumeFound) {
@@ -151,7 +238,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							lorryVolume = Short.parseShort(tokens[1]);
-							if (!lorryVolumeFound)		lorryVolumeFound = true;
+							lorryVolumeFound = true;
 						}  
 						break;
 					
@@ -163,7 +250,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							lorryMaxLoad = Short.parseShort(tokens[1]);
-							if (!lorryMaxLoadFound)		lorryMaxLoadFound = true;
+							lorryMaxLoadFound = true;
 						}
 						break;
 					
@@ -175,7 +262,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							binServiceTime = Float.parseFloat(tokens[1]);
-							if (!binServiceTimeFound)		binServiceTimeFound = true;
+							binServiceTimeFound = true;
 						}
 						break;
 	
@@ -187,7 +274,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							binVolume = Float.parseFloat(tokens[1]);
-							if (!binVolumeFound)		binVolumeFound = true;	
+							binVolumeFound = true;	
 						}				
 						break;
 					
@@ -200,13 +287,13 @@ public class Simulator {
 								for (int i = 2; i < tokensLen; i++) {
 									float ddr = Float.parseFloat(tokens[i]);
 									disposalDistrRateExp.add(ddr);
-									if (!disposalDistrRateFound)		disposalDistrRateFound = true;
-									if (!isExperiment)		isExperiment = true;
+									isExperiment = true;
 								}
+								disposalDistrRateFound = true;
 							} else {
 								if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 								disposalDistrRate = Float.parseFloat(tokens[1]);
-								if (!disposalDistrRateFound)		disposalDistrRateFound = true;
+								disposalDistrRateFound = true;
 							}
 						}  else throw new InvalidInputFileException("Invalid input format in this line: " + line);
 					break;
@@ -220,13 +307,13 @@ public class Simulator {
 								for (int i = 2; i < tokensLen; i++) {
 									short dds = Short.parseShort(tokens[i]);
 									disposalDistrShapeExp.add(dds);
-									if (!disposalDistrShapeFound)		disposalDistrShapeFound = true;
-									if (!isExperiment)		isExperiment = true;
+									isExperiment = true;
 								}
+								disposalDistrShapeFound = true;
 							} else {
 								if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 								disposalDistrShape = Short.parseShort(tokens[1]);
-								if (!disposalDistrShapeFound)		disposalDistrShapeFound = true;
+								disposalDistrShapeFound = true;
 							}
 						}  else throw new InvalidInputFileException("Invalid input format in this line: " + line);
 					break;
@@ -239,7 +326,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							bagVolume = Float.parseFloat(tokens[1]);
-							if (!bagVolumeFound)		bagVolumeFound = true;
+							bagVolumeFound = true;
 						}  
 						break;
 					
@@ -251,7 +338,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							bagWeightMin = Float.parseFloat(tokens[1]);
-							if (!bagWeightMinFound)		bagWeightMinFound = true;
+							bagWeightMinFound = true;
 						}  
 						break;
 					
@@ -263,7 +350,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							bagWeightMax = Float.parseFloat(tokens[1]);
-							if (!bagWeightMaxFound)		bagWeightMaxFound = true;
+							bagWeightMaxFound = true;
 						}  
 						break;
 					
@@ -275,7 +362,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							stopTime = Float.parseFloat(tokens[1]);
-							if (!stopTimeFound)		stopTimeFound = true;
+							stopTimeFound = true;
 						}  
 						break;
 					
@@ -287,7 +374,7 @@ public class Simulator {
 						} else {
 							if (tokensLen > 2)		System.out.println("Warning: too many inputs in this line: " + line);
 							warmUpTime = Float.parseFloat(tokens[1]);
-							if (!warmUpTimeFound)		warmUpTimeFound = true;
+							warmUpTimeFound = true;
 						}
 						break;
 						
@@ -349,6 +436,11 @@ public class Simulator {
 		for (ServiceArea sa : serviceAreas) sa.print();
 		System.out.println(stopTime);
 		System.out.println(warmUpTime);
+		
+		System.out.println(isExperiment);
+		for (Float sa : disposalDistrRateExp) System.out.println("disposalDistrRateExp: " + sa);
+		for (Short sa : disposalDistrShapeExp) System.out.println("disposalDistrShapeExp: " + sa);
+		for (Float sa : serviceFreqExp) System.out.println("serviceFreqExp: " + sa);
 		
     } // end of main method
 	
