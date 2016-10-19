99e4da2e6df0360bbdf1565e194c4602102064f7
Isabella Chan <isabellachan426@gmail.com>
2016-10-19 10:08:50 +0100

    added variables and methods

--- cslp/src/cslp/ServiceArea.java
+++ cslp/src/cslp/ServiceArea.java
@@ -3,11 +3,13 @@ package cslp;
 import java.util.Arrays;
 
 public class ServiceArea {
-	public short areaIdx; // unit8_t
-	public float serviceFreq;
-	public float thresholdVal;
-	public int noBins; // unit16_t
-	public short[][] roadsLayout; // int8_t
+	private static short areaIdx; // unit8_t
+	private static float serviceFreq;
+	private static float thresholdVal;
+	private static int noBins; // unit16_t
+	private static short[][] roadsLayout; // int8_t
+	private static Bin[] bins = new Bin[65535];
+	private static Lorry lorry;
 	
 	public ServiceArea(short areaIdx, float serviceFreq,
 			float thresholdVal, int noBins, short[][] roadsLayout) {
@@ -18,6 +20,16 @@ public class ServiceArea {
 		this.roadsLayout = roadsLayout;
 	}
 	
+	public void assignLorry(Lorry lorry) {
+		this.lorry = lorry;
+	}
+	
+	public void createBins(Bin bin) {
+		for (int i = 0; i < noBins; i++) {
+			bins[i] = bin;
+		}
+	}
+	
 	public void print() {
 		String s = "areaIdx= "+String.valueOf(areaIdx)+" serviceFreq= "+String.valueOf(serviceFreq)+
 				" thresholdVal= "+String.valueOf(thresholdVal)+" noBins= "+String.valueOf(noBins);
@@ -25,5 +37,10 @@ public class ServiceArea {
 		System.out.println("roadsLayout");
 		System.out.println(Arrays.deepToString(roadsLayout));
 	}
+	
+	/*
+	public Bin[] getBins() {
+		return bins;
+	}*/
 	
 }
