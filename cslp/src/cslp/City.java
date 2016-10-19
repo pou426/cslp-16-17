57d06334a59b1df76c1fd706e64a75062e6569c2
Isabella Chan <isabellachan426@gmail.com>
2016-10-19 10:07:40 +0100

    updated city class

--- cslp/src/cslp/City.java
+++ cslp/src/cslp/City.java
@@ -1,10 +1,10 @@
 package cslp;
 
+import java.util.ArrayList;
+
 public class City {
-	// Number of service areas (max 65,535)
-	public static int noAreas; // uint8_t
 	
-	public City(int noAreas) {
-		City.noAreas = noAreas;
-	}
+		private static short noAreas;
+		private static ArrayList<ServiceArea> serviceAreas = new ArrayList<ServiceArea>();
+		
 }
