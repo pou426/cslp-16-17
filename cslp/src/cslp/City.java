03d247b9330dc6abaa75845edee554b45c6a6f4e
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-24 14:03:56 +0100

    added service area instances to city

--- cslp/src/cslp/City.java
+++ cslp/src/cslp/City.java
@@ -7,24 +7,24 @@ import java.util.PriorityQueue;
 
 public class City {
 	
-		protected static short noAreas;
-		protected static ArrayList<ServiceArea> serviceAreas = new ArrayList<ServiceArea>();
-		protected static PriorityQueue<Event> pq = new PriorityQueue<Event>();
-		
-		// keep track of time
-		protected int currTime;
-		protected static float stopTime; // short or int or float?
-		protected static float warmUpTime; // short or int or float?
-
-		public City() {
-			this.currTime = 0;
-		}
-		
-		public void setTime(int delay) {
-			this.currTime += delay;
-		}
-		
-		public int getTime() {
-			return currTime;
-		}
+	// area specifications
+	protected static short noAreas;
+	protected static ArrayList<ServiceArea> serviceAreas = new ArrayList<ServiceArea>();
+	protected static PriorityQueue<Event> pq = new PriorityQueue<Event>();
+	
+	// keep track of time
+	protected int currTime;
+	protected static float stopTime; // short or int or float?
+	protected static float warmUpTime; // short or int or float?
+	
+	public City() {
+		this.currTime = 0;
+	}
+	
+	public void setTime(int delay) {
+		this.currTime += delay;
+	}
+	public int getTime() {
+		return currTime;
+	}
 }
