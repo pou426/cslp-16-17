21a3a43855655162b33a30fd61bdbd6577e67029
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-29 15:59:12 +0100

    redundant Clock class and update static variables in Random

--- cslp/src/cslp/Random.java
+++ cslp/src/cslp/Random.java
@@ -1,8 +1,11 @@
 package cslp;
 
 public class Random {
+	private static float disposalDistrRate; // expressed per hour
+	private static short disposalDistrShape;
+
 	// this class computes the erlangk distributino
-	public static int erlangk(float disposalDistrRate, short disposalDistrShape) {
+	public static int erlangk() {
 		double erlangk = 0;
 		for (int i = 0; i < disposalDistrShape; i++) {
 			double rand = Math.random();
@@ -14,5 +17,13 @@ public class Random {
 		double convertToSec = erlangk*60*60;
 		int finalresult = (int) Math.round(convertToSec);
 		return finalresult;
+	}
+	
+	public static void setDisposalDistrRate(float disposalDistrRate) {
+		Random.disposalDistrRate = disposalDistrRate;
+	}
+	
+	public static void setDisposalDistrShape(short disposalDistrShape) {
+		Random.disposalDistrShape = disposalDistrShape;
 	}
 }
