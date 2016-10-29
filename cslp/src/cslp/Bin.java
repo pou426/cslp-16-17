2fbb7fbdf626023b59ed52b5c1d002be35427fc2
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-29 23:44:43 +0100

    add outputs

--- cslp/src/cslp/Bin.java
+++ cslp/src/cslp/Bin.java
@@ -7,40 +7,57 @@ public class Bin {
 	private float wasteWeight;
 	private short areaIdx;
 	private int binIdx;
+	private float thresholdVal;
 	private boolean isOverflow;
-		
-	public Bin(short areaIdx, int binIdx) {
+	private boolean isExceedThreshold;
+
+	
+	public Bin(short areaIdx, int binIdx, float thresholdVal) {
 		this.wasteVolume = 0;
 		this.wasteWeight = 0;
 		this.areaIdx = areaIdx;
 		this.binIdx = binIdx;
+		this.thresholdVal = thresholdVal;
 		this.isOverflow = false;
+		this.isExceedThreshold = false;
 	}
 	
 	// output event
 	public void disposeBag(Event e) {
 		// do something with simulator like add an event???
 		Bag bag = new Bag();
-		int currTime = e.getTime();
 		float bagWeight = bag.getWeight();
-		if (this.isOverflow()) {
-			System.out.println("bin overflowed so no event generated anymore");
+		if (isOverflow) {
+			//System.out.println("bin overflowed so no event generated anymore");
 		} else {
 			this.wasteVolume += Bag.getBagVolume();
 			this.wasteWeight += bagWeight;
-			String s = e.timeToString() + " -> " + "bag weighing "+bagWeight+" disposed of at bin "+areaIdx+"."+binIdx;
-			System.out.println(s);
+			String disposalString = e.timeToString() + " -> bag weighing "+bagWeight+" disposed of at bin "+areaIdx+"."+binIdx;
+			System.out.println(disposalString);
+			String binStatusString = e.timeToString() + " -> load of bin "+areaIdx+"."+binIdx+" became "+wasteWeight+" and contents volume "+wasteVolume+" m^3";
+			System.out.println(binStatusString);
+			double currentOccupancy = wasteVolume/binVolume;
+			if (currentOccupancy > thresholdVal) {
+				isExceedThreshold = true;
+				String exceedThresholdString = e.timeToString() + " -> occupancy threshold of bin "+areaIdx+"."+binIdx+" exceeded";
+				System.out.println(exceedThresholdString);
+			}
+			if (wasteVolume >= binVolume) {
+				isOverflow = true;
+				String overflowString = e.timeToString() + " -> "+ "bin "+areaIdx+"."+binIdx+" overflowed";
+				System.out.println(overflowString);
+			}
 		}
 	}
-	
+	/*
 	public boolean isOverflow() {
 		this.isOverflow = this.wasteVolume >= binVolume;
 		return isOverflow;
-	}
+	}*/
 	
 	// TODO check decimal places or data type 
 	public double currentOccupancy() {
-		return this.wasteVolume/binVolume;
+		return wasteVolume/binVolume;
 	}
 	
 	public double currentWeight() {
