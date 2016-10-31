e912093cccb1fa9044be5552ed90d891654d3514
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-10-31 20:33:42 +0000

    updated DisposalEvent.java

--- cslp/src/cslp/DisposalEvent.java
+++ cslp/src/cslp/DisposalEvent.java
@@ -1,73 +1,21 @@
 package cslp;
 
-import org.junit.Test;
+public class DisposalEvent extends AbstractEvent {
 
-public class Event implements Comparable {
-
-	private static float stopTime;
-	public static void setStopTime(float stopTime) {
-		Event.stopTime = stopTime;
-	}
-
-	private int time;
-	public int getTime() {
-		return time;
-	}
-	public String timeToString() {
-		int secCount = time;
-		int minCount = 0;
-		int hrCount = 0;
-		int dayCount = 0;
-		while (secCount >= 60) {
-			secCount -= 60;
-			minCount += 1;
-		}
-		while (minCount >= 60) {
-			minCount -= 60;
-			hrCount += 1;
-		}
-		while (hrCount >= 24) {
-			hrCount -= 24;
-			dayCount += 1;
-		}
-		String sec = String.format("%02d", secCount);
-		String min = String.format("%02d", minCount);
-		String hr = String.format("%02d", hrCount);
-		String day = String.format("%02d", dayCount);
-		String result = day+":"+hr+":"+min+":"+sec;
-		return result;
-	}
-	public Event(int time) {
-		this.time = time;
-	}
-	
-	
-	
-	
 	// disposal event
 	private Bin bin;
-	public Event(int time, Bin bin) {
-		this.time = time;
+	public DisposalEvent(int time, Bin bin) {
+		setTime(time);
 		this.bin = bin;
 	}
+	
 	public void execute(Simulator simulator) {
     	this.bin.disposeBag(this); 
-    	this.time += Random.erlangk();
-    	if (this.time < stopTime) {
+    	int currTime = getTime();
+    	int newTime = currTime + Random.erlangk();
+    	setTime(newTime);
+    	if (getTime() < getStopTime()) {
     		simulator.insert(this);
     	}
     }
-
-	@Override
-	public int compareTo(Object anotherEvent) {
-		// TODO Auto-generated method stub
-		if (!(anotherEvent instanceof Event)) {
-			throw new ClassCastException("An Event object expected.");
-		}
-		int anotherEventTime = ((Event) anotherEvent).getTime();
-		if (this.time == anotherEventTime)		return 0;
-		else if (this.time > anotherEventTime)	return 1;
-		else return -1;
-	}
-
 }
