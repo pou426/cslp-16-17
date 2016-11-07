1b62c6e1eeef8413392e5099f457bc3e38ab681e
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-11-07 12:28:21 +0000

    split script into three parts to check argument lengths, valid and invalid text files

--- simulate.sh
+++ simulate.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 # This script will be used to launch simulations 
 
-if [[ -e $1 ]] ; then
+if [[ $# == 0 ]]; then
+	#echo '"$1" does not exist; exiting.'
+	# display usage information
+	echo "Usage information: "
+	echo "./simulate.sh <input_filename>"
+	#echo "Run code using ./simulate.sh <input_file_name> [OPTIONS]"
+
+	exit 1
+
+elif [[ -e $1 ]] ; then
 	abs_path="$(cd "$(dirname $1)" && pwd)/$(basename "$1")"
 
 	cd cslp/bin
 	java cslp/Simulator $abs_path
 	# echo $abs_path
-
+	exit 1
+	
 else
-	#echo '"$1" does not exist; exiting.'
-	# display usage information
-	echo "Usage information: "
-	echo "./simulate.sh <input_filename>"
+	echo "File does not exist"
+	echo "Simulation will terminate"
 	#echo "Run code using ./simulate.sh <input_file_name> [OPTIONS]"
 
 	exit 1
