68aa2527dd864ff9f63fd49feee84490cb0c41ed
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-11-10 21:54:04 +0000

    clean up comments

--- simulate.sh
+++ simulate.sh
@@ -2,12 +2,9 @@
 # This script will be used to launch simulations 
 
 if [[ $# == 0 ]]; then
-	#echo '"$1" does not exist; exiting.'
 	# display usage information
 	echo "Usage information: "
 	echo "./simulate.sh <input_filename>"
-	#echo "Run code using ./simulate.sh <input_file_name> [OPTIONS]"
-
 	exit 1
 
 elif [[ -e $1 ]] ; then
@@ -15,19 +12,11 @@ elif [[ -e $1 ]] ; then
 
 	cd cslp/bin
 	java cslp/Simulator $abs_path
-	# echo $abs_path
 	exit 1
 	
 else
 	echo "File does not exist"
 	echo "Simulation will terminate"
-	#echo "Run code using ./simulate.sh <input_file_name> [OPTIONS]"
 
 	exit 1
 fi
-
-
-# usage information:
-#$ ./simulate.sh
-#Usage: ./simulate.sh <input_filename> [debug=on|off]
-#Default option: debug=off </input_filename>
