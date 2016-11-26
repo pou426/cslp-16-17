bea02ba8766d990bc0e9fa5205c6905f8a3916d9
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-11-26 17:46:48 +0000

    extends serviceAreaInfo class

--- cslp/src/cslp/ServiceArea.java
+++ cslp/src/cslp/ServiceArea.java
@@ -1,90 +1,41 @@
 package cslp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 /**
  * ServiceArea class stores information for a service area
  *
  */
-public class ServiceArea {
+public class ServiceArea extends ServiceAreaInfo {
 	
-	private short areaIdx; 
-	private float serviceFreq; // eg. serviceFreq 0.0625 means 0.0625 trips per hour
-	private int serviceInterval; // in second
-	private float thresholdVal;
-	private int noBins;
-	private int[][] roadsLayout = null; 
+	private int serviceInterval; // in second (time interval between scheduled bin servicing events
 	private Bin[] bins = null;
 	private Lorry lorry; // each service area has its own lorry
-	private boolean isServicing; // carrying out service event?
-	ArrayList<BinServiceEvent> waitingServiceEvents = new ArrayList<BinServiceEvent>();
 	
 	public ServiceArea(short areaIdx, float serviceFreq, float thresholdVal, 
 			int noBins, int[][] roadsLayout) {
-		this.areaIdx = areaIdx;
-		this.serviceFreq = serviceFreq;
+		
+		super(areaIdx, serviceFreq, thresholdVal, noBins, roadsLayout);
+		
 		this.serviceInterval = Math.round((1/serviceFreq)*60*60); // in second
-		this.thresholdVal = thresholdVal;
-		this.noBins = noBins;
-		this.roadsLayout = roadsLayout;
 		Bin[] bins = new Bin[noBins];
-		this.bins = bins;
-		for (int i = 0; i < noBins; i++) {		// create bins
-			this.bins[i] = new Bin(areaIdx, i+1, thresholdVal);
+		for (int i = 0; i < noBins; i++) {
+			bins[i] = new Bin(this, i+1);
 		}
-		this.lorry = null;
-		this.isServicing = false;
+		this.bins = bins;
+		this.lorry = new Lorry(this);
 	}
 	
-	public void setLorry(Lorry lorry) {
-		this.lorry = lorry;
+	public void computePath() {
+		// compute best path here 
 	}
+	
+	
 	public Lorry getLorry() {
 		return this.lorry;
 	}
-	public void setIsServicing(boolean b) {
-		this.isServicing = b;
-	}
-	public boolean getIsServicing() {
-		return this.isServicing;
-	}
-	/**
-	 * return a list of bins in this service area
-	 * 
-	 * @return Bin[]	an array of all bins in that service area
-	 */
 	public Bin[] getBins() {
-		return bins;
-	}
-	public short getAreaIdx() {
-		return areaIdx;
-	}
-	public float getServiceFreq() {
-		return this.serviceFreq;
-	}
-	public float getThresholdVal() {
-		return this.thresholdVal;
+		return this.bins;
 	}
 	public int getServiceInterval() {
 		return this.serviceInterval;
-	}
-	/** 
-	 * print service area information for checking
-	 */
-	public String toString() {
-		String a = "areaIdx = "+String.valueOf(areaIdx)+" serviceFreq (in hour) = "+String.valueOf(serviceFreq)
-				+" thresholdVal = "+String.valueOf(thresholdVal)+" noBins = "+String.valueOf(noBins);
-		String b = "\nroadsLayout (in sec) = ";
-		String c = "\n";
-		for (int i = 0; i < roadsLayout.length; i++) {
-			for (int j = 0; j < roadsLayout[0].length; j++) {
-				c = c + roadsLayout[i][j] + " ";
-			}
-			if (!(i==roadsLayout.length-1)) c = c + "\n";
-		}
-		String result = a+b+c;
-		return result;
-	}
-	
+	}	
 }
