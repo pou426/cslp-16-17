261bc33099942ae45dc3c09fcc5ae135a305705b
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-12-07 23:36:04 +0000

    changed exit code

--- simulate.sh
+++ simulate.sh
@@ -5,14 +5,14 @@ if [[ $# == 0 ]]; then
 	# display usage information
 	echo "Usage information: "
 	echo "./simulate.sh <input_filename>"
-	exit 1
+	# exit 1
 
 elif [[ -e $1 ]] ; then
 	abs_path="$(cd "$(dirname $1)" && pwd)/$(basename "$1")"
 
 	cd cslp/bin
 	java cslp/Simulator $abs_path
-	exit 1
+	# exit 1
 	
 else
 	echo "File does not exist"
