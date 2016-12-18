0b3f6f73a18164beaebf0a51275a7fa0fbd22013
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-12-18 00:59:08 +0000

    changed from PQueue to arraylist

--- cslp/src/cslp/NearestNeighbour.java
+++ cslp/src/cslp/NearestNeighbour.java
@@ -7,67 +7,84 @@ import java.util.ArrayList;
  * @author home
  *
  */
+
 public class NearestNeighbour {
 
-	public ArrayList<Integer> getRoute(int[][] routeLayout) {
-		ArrayList<Integer> route = new ArrayList<Integer>();
+	public int[] getRoute(int[][] routeLayout) {
+		
+		// returns a route that include depots (begining and end)
+		
+		// do checking...
+		int routeLen = routeLayout.length+1;
+		int[] route = new int[routeLen];
 		// starts and ends at 0
 		int len = routeLayout.length;
 		boolean[] visited = new boolean[len];
 		for (boolean b : visited) {
 			b = false;
 		}
+		
 		visited[0] = true;
 		
 		boolean keepGoing = true;
 		
 		int currLocation = 0; // always start at depot
-		route.add(currLocation);
+		int idx = 0;
+
+		route[idx] = currLocation;
+		idx++;
 		
 		int minDist = Integer.MAX_VALUE;
 		int nn = -1;
 		
 		while (keepGoing) {
-//			System.out.println("currLocation : "+currLocation);
 			for (int i = 0; i < len; i++) {
-//				System.out.println("current i : "+i);
-				if (i == currLocation) {
-					// ignore 
-//					System.out.println("(i == currLocation)");
-				}
-				else if (i == 0) {
-					// ignore
-//					System.out.println("(i == 0)");
-				}
-				else if (visited[i]) {
-//					System.out.println(i+" has been visited.");
-				}
-				else if (!visited[i]) {
-//					System.out.println("Min dist = "+minDist);
-//					System.out.println("considering dist = "+routeLayout[currLocation][i]);
+				if ((i!=currLocation) && (i!=0) && !visited[i]) {
 					if (routeLayout[currLocation][i] < minDist) {
 						minDist = routeLayout[currLocation][i];
 						nn = i;
 					}
-//					System.out.println("current nn : "+nn);
 				}
 			}
-			if (nn == -1) { // go to nearest neighbour and keep searching
-				// finish and go back to depot?
+			if (nn == -1) { 
 				nn = 0;
-				route.add(nn);
+				route[idx] = nn;
 				keepGoing = false;
 			} else {
-				route.add(nn);
+				route[idx] = nn;
 				currLocation = nn;
 				visited[nn] = true;
 			}
 			minDist = Integer.MAX_VALUE;
 			nn = -1;
+			idx++;
 		}
-		
-		return route;
+		System.out.print("Route is: ");
+		for (int r : route) {
+			System.out.print(r+" -> ");
+		}
+		System.out.println();
+		return route; // index in the subgraph form
 	}
+	
+	// input ServiceArea instance so directly modify the sa's serviceQueue
+	// instead of creating another arraylist?
+	public ArrayList<Integer> getServiceQueue(int[][] routeLayout, int[] requiredVertices) {
+		int[] route = getRoute(routeLayout);
+		
+		int len = route.length - 2; // remove depots at the start and end
+		ArrayList<Integer> serviceQueue = new ArrayList<>();
+		System.out.print("serviceQueue: ");
+		for (int i = 1; i < route.length-1; i++) {
+			int subGraphLocation = route[i];
+			int location = requiredVertices[subGraphLocation];
+			serviceQueue.add(location);
+			System.out.print(location+" -> ");
+		}
+		System.out.println();
+		return serviceQueue;
+	}
+	
 	
 	public static void main(String[] args) {
 		int[][] routeLayout = { { 0, 1, 2, 3, 5},
@@ -76,8 +93,8 @@ public class NearestNeighbour {
 								{ 1, 1, 4, 0, 2},
 								{ 3, 1, 4, 1, 0}};
 		NearestNeighbour nearestNeighbour = new NearestNeighbour();
-		ArrayList<Integer> route = nearestNeighbour.getRoute(routeLayout);
-		System.out.println("route is : ");
+		int[] route = nearestNeighbour.getRoute(routeLayout);
+		System.out.println("route : ");
 		for (int i : route) {
 			System.out.print(i+" ");
 		}
