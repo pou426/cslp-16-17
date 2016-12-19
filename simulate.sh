b62254c6d543626d697f48aba081dacb93012335
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-12-19 17:40:58 +0000

    include flags for logger output to console

--- simulate.sh
+++ simulate.sh
@@ -4,14 +4,16 @@
 if [[ $# == 0 ]]; then
 	# display usage information
 	echo "Usage information: "
-	echo "./simulate.sh <input_filename>"
+	echo "./simulate.sh <input_filename> <flag1>"
+	echo "where <flag1> = 'yes' if user wants to output logging information to console" 
+	echo "see ./output_files/output.txt for program outputs."
 	# exit 1
 
 elif [[ -e $1 ]] ; then
 	abs_path="$(cd "$(dirname $1)" && pwd)/$(basename "$1")"
 
 	cd cslp/bin
-	java cslp/Simulator $abs_path
+	java cslp/Simulator $abs_path $2 > ../../output_files/output.txt
 	# exit 1
 	
 else
