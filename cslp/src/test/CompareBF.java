2728e49b7cad19592bbb30c1523fd0e8b3956bd1
Isabella Chan <s1330027@sms.ed.ac.uk>
2016-12-19 17:40:31 +0000

    brute force testing

---
+++ cslp/src/test/CompareBF.java
@@ -0,0 +1,27 @@
+package test;
+import java.util.Arrays;
+
+import cslp.BruteForce;
+public class CompareBF {
+
+	public void testing(int n) {
+		BruteForce bf = new BruteForce();
+		int[][] matrix = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				matrix[i][j] = (int) Math.floor(Math.random()*10);
+				System.out.println(matrix[i][j]);
+			}
+		}
+		int[] route = bf.getRoute(matrix);
+		System.out.println(Arrays.toString(route));
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		CompareBF cbf = new CompareBF();
+		cbf.testing(10); // acceptable performance
+		
+//		cbf.testing(13); // gets too slow from 13 onwards
+		
+	}
+}
